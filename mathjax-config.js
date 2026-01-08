68d4e38b29436302fb6ef57fe553dbfede313dc3
skrepka <>
2026-01-09 01:39:05 +0300

    попытка фикса mathjax

---
+++ mathjax-config.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+    ignoreHtmlClass: 'tex2jax_ignore'
+  }
+};
